dcb57a83053170bfa9ced948691e431c63407e62
princealiza <162723343+princealiza@users.noreply.github.com>
2024-04-08 17:57:00 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+// Printing a Wellcome Message
+console.log("\n\tWellcome To \'Aliza Fatima\' -CLI Simple Calculator\n");
+//Asking Questions from users through inquirer
+let answers = await inquirer.prompt([
+    { message: "Enter First Number", type: "number", name: "firstNumber" },
+    { message: "Enter Second Number", type: "number", name: "secondNumber" },
+    {
+        message: "Select one Operator to perform Operations",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtractions", "Multiplication", "Division"],
+    }
+]);
+//Conditional statements If-Else
+if (answers.operator === "Addition") {
+    console.log(answers.firstNumber + answers.secondNumber);
+}
+else if (answers.operator === "Subtractions") {
+    console.log(answers.firstNumber - answers.secondNumber);
+}
+else if (answers.operator === "Multiplication") {
+    console.log(answers.firstNumber * answers.secondNumber);
+}
+else if (answers.operator === "Division") {
+    console.log(answers.firstNumber / answers.secondNumber);
+}
+else {
+    console.log("Invalid Input");
+}
